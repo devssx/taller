4c39dad116c5a2f1f99af1c2affa9b5928ac90ba
ssx <ssx@live.com.mx>
2020-06-14 21:23:21 -0700

    +notification area

--- resources/js/components/Tools/Notifications.vue
+++ resources/js/components/Tools/Notifications.vue
@@ -1,17 +1,27 @@
 <template>
   <div>
     <el-badge
-      v-if="clients.length>0"
+      v-show="clients.length>0"
       :value="clients.length"
       style="margin-top:0px;margin-right:0px;line-height:30px;"
     >
-      <el-popover placement="bottom" title width="400" trigger="click" content="Sin Notificaciones">
+      <el-popover placement="bottom" title width="400" trigger="click">
+        <el-row v-for="(c, index) in clients" :key="c.id" style="padding:1px">
+          <el-alert
+            :title="c.name"
+            :description="c.reminder"
+            type="info"
+            @close="deleteNotification(index)"
+          ></el-alert>
+        </el-row>
+        <span v-show="clients.length==0" style="padding-left: 140px;">Sin Notificaciones</span>
         <a href="#" slot="reference" style="color:white;">Notificaciones</a>
       </el-popover>
     </el-badge>
     <!-- Si no hay -->
-    <el-popover placement="bottom" title width="400" trigger="click" content="Sin Notificaciones">
-      <a v-if="clients.length==0" href="#" slot="reference" style="color:white;">Notificaciones</a>
+    <el-popover placement="bottom" title width="400" trigger="click">
+      <span v-show="clients.length==0" style="padding-left: 140px;">Sin Notificaciones</span>
+      <a v-show="clients.length==0" href="#" slot="reference" style="color:white;">Notificaciones</a>
     </el-popover>
   </div>
 </template>
@@ -19,21 +29,28 @@
 <script>
 export default {
   data() {
-    return { clients: [] };
+    return {
+      clients: []
+    };
   },
   mounted: function() {
     this.loadReminders("/api/clients?reminders=1");
   },
   methods: {
-    delete() {},
+    deleteNotification(index) {
+      this.clients.splice(index, 1);
+    },
     loadReminders(url) {
       var $this = this;
       axios.get(url).then(function(response) {
-        $this.clients = response.data;
+        //$this.clients = response.data;
         var n = 1;
-        $this.clients.forEach(c => {
+        response.data.forEach(c => {
           n++;
           setTimeout(function() {
+            // inserta reminder en la lista
+            $this.clients.push(c);
+            // muestra reminder
             $this.show(c.name, c.reminder);
           }, 1000 * n);
         });
