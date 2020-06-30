8a462d6873f3bc455ee677e36bc94cd0a4be6bcc
ssx <ssx@live.com.mx>
2020-06-29 21:10:02 -0700

    new controller

---
+++ app/Http/Controllers/PayrollCommentController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\PayrollComment;
+use Illuminate\Http\Request;
+
+class PayrollCommentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\PayrollComment  $payrollComment
+     * @return \Illuminate\Http\Response
+     */
+    public function show(PayrollComment $payrollComment)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\PayrollComment  $payrollComment
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(PayrollComment $payrollComment)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\PayrollComment  $payrollComment
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, PayrollComment $payrollComment)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\PayrollComment  $payrollComment
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(PayrollComment $payrollComment)
+    {
+        //
+    }
+}
