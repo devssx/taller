fe6ec13a5e0e592acad64919d57a20e6a7b63fac
ssx <ssx@live.com.mx>
2020-08-13 20:45:39 -0700

    *roles

--- resources/views/roles/index.blade.php
+++ resources/views/roles/index.blade.php
@@ -5,20 +5,20 @@
         <el-col :span="19">
             <h2>Roles</h2>
         </el-col>
-        @can('listar roles')
+        @can('agregar roles')
         <create-roles :permissions="{{ $permissions->toJson()}}"></create-roles>
         @endcan
     </el-row>
     <el-row type="flex">
         <el-col :span="24">
-            @can('listar roles')
+            {{-- @can('listar roles') --}}
             <list-roles :permissions="{{ $permissions->toJson()}}"></list-roles>
-            @endcan
+            {{-- @endcan
             @cannot('listar roles')
             <br>
             <el-alert title="Acceso no autorizado." type="error" closable="false" show-icon>
             </el-alert>
-            @endcan
+            @endcan --}}
         </el-col>
     </el-row>
 </el-main>
