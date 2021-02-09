adbe40d8af5441709e74e479fa630b9427805187
Salomon <ssx@live.com.mx>
2021-02-08 19:58:52 -0800

    *recibos

--- resources/js/mixins/commons.js
+++ resources/js/mixins/commons.js
@@ -236,11 +236,20 @@ export default {
                 type: 'warning'
             }).then(() => callback()).catch(() => { });
         },
-        getDirection(workshop) {
-            var dir = `SOFIA GALLEGOS RAMIREZ GARS970103QR3 Rio Culiacán Esq. Josefa Ortiz de Dguez. #898 Col. Prohogar C.P.21240 Mexicali B.C. México`;
+        getAddress(workshop) {
+            // var dir = `SOFIA GALLEGOS RAMIREZ GARS970103QR3 Rio Culiacán Esq. Josefa Ortiz de Dguez. #898 Col. Prohogar C.P.21240 Mexicali B.C. México`;
+            // var cta = `BANORTE CTA. 0278268809 CLAVE INT. 072020002782688099`;
+            // var tel = `TEL. 333 0212  CEL: 686160 7472`;
+
+            var dir = `SOFIA GALLEGOS RAMIREZ GARS970103QR3 Rio Culiacán Esq. Juan de la barrera #894 Col. Prohogar C.P.21240 Mexicali B.C. México`;
             var cta = `BANORTE CTA. 0278268809 CLAVE INT. 072020002782688099`;
-            var tel = `TEL. 333 0212  CEL: 686160 7472`;
+            var tel = `TEL. 6865236721 / Cel. 686190 9481`;
             return [dir, cta, tel];
+        },
+        getSucAddress(workshop) {
+            var a = `Rio Culiacán Esq. Josefa Ortiz de Dominguez. #898 Prohogar Mexicali B.C.  TEL. 333 0212   CEL: 686160 7472`;
+            var b = `San Pedro Mezquital y calle séptima #2805 Nuevo Mexicali, B.C. Tel. 334 1750 / Tel. 334 1761`;
+            return [a, b];
         },
         createReceipt(currentSale, receiptImg, myCanvas, format) {
             var $this = this;
@@ -257,17 +266,29 @@ export default {
             // Header Direccion del taller
             $this.context.font = "20px Calibri";
             $this.context.fillStyle = "black";
-            var direction = $this.getDirection(currentSale.workshop_id);
-            $this.wrapText($this.context, direction[0], 275, 80, 580, 22);
-            $this.wrapText($this.context, direction[0], 275, 80 + 678, 580, 22);
+            var address = $this.getAddress(currentSale.workshop_id);
+            $this.wrapText($this.context, address[0], 275, 80, 580, 22);
+            $this.wrapText($this.context, address[0], 275, 80 + 678, 580, 22);
             $this.context.textAlign = "center";
-            $this.context.fillText(direction[1], 555, 130);
-            $this.context.fillText(direction[2], 555, 154);
-            $this.context.fillText(direction[1], 555, 130 + 678);
-            $this.context.fillText(direction[2], 555, 154 + 678);
+            $this.context.fillText(address[1], 555, 130);
+            $this.context.fillText(address[2], 555, 154);
+            $this.context.fillText(address[1], 555, 130 + 678);
+            $this.context.fillText(address[2], 555, 154 + 678);
+
+            $this.context.font = "12px Calibri";
+            var sucAddress = $this.getSucAddress(currentSale.workshop_id);
+            $this.context.fillText(`Sucursal:`, 320, 172);
+            $this.context.fillText(`Sucursal:`, 850, 172);
+            $this.context.fillText(sucAddress[0], 320, 185);
+            $this.context.fillText(sucAddress[1], 850, 185);
+            $this.context.fillText(`Sucursal:`, 320, 172 + 678);
+            $this.context.fillText(`Sucursal:`, 850, 172 + 678);
+            $this.context.fillText(sucAddress[0], 320, 185 + 678);
+            $this.context.fillText(sucAddress[1], 850, 185 + 678);
 
             // fecha de recibo
             $this.context.textAlign = "left";
+            $this.context.font = "24px Calibri";
             let doneOn = new Date();
             if (currentSale.done_on) {
                 try {
@@ -510,11 +531,18 @@ export default {
             // Header Direccion del taller
             $this.context.font = "20px Calibri";
             $this.context.fillStyle = "black";
-            var direction = $this.getDirection(currentSale.workshop_id);
-            $this.wrapText($this.context, direction[0], 225, 140, 660, 22);
+            var address = $this.getAddress(currentSale.workshop_id);
+            $this.wrapText($this.context, address[0], 225, 140, 650, 22);
             $this.context.textAlign = "center";
-            $this.context.fillText(direction[1], 550, 190);
-            $this.context.fillText(direction[2], 550, 214);
+            $this.context.fillText(address[1], 550, 190);
+            $this.context.fillText(address[2], 550, 214);
+
+            $this.context.font = "12px Calibri";
+            var sucAddress = $this.getSucAddress(currentSale.workshop_id);
+            $this.context.fillText(`Sucursal:`, 320, 232);
+            $this.context.fillText(`Sucursal:`, 850, 232);
+            $this.context.fillText(sucAddress[0], 320, 245);
+            $this.context.fillText(sucAddress[1], 850, 245);
 
             $this.context.textAlign = "left";
             $this.context.font = "24px Calibri";
